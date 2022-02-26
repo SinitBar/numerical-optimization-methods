633d247da880941ad64a8e2a4011f1d562e99382
SinBar <varka.sinitsckaya@yandex.ru>
2022-02-26 22:40:00 +0300

    Update laba3_newton.py

--- laba3_newton.py
+++ laba3_newton.py
@@ -1,11 +1,11 @@
 import numpy as np
 
-f = lambda x: x ** 2 - 10 * np.cos(0.3 * np.pi * x) - 20
-df = lambda x: 2 * x + 3 * np.sin(0.3 * np.pi * x) * np.pi
-ddf = lambda x: 2 + 0.9 * np.cos(0.3 * np.pi * x) * np.pi * np.pi
-tol = 0.0000000001 # delta = 10^(-10)
-interval = [-2, 7]
-x0 = 1.3
+#f = lambda x: x ** 2 - 10 * np.cos(0.3 * np.pi * x) - 20
+#df = lambda x: 2 * x + 3 * np.sin(0.3 * np.pi * x) * np.pi
+#ddf = lambda x: 2 + 0.9 * np.cos(0.3 * np.pi * x) * np.pi * np.pi
+#tol = 0.0000000001 # delta = 10^(-10)
+#interval = [-2, 7]
+#x0 = 1.3
 
 def nsearch(interval, tol, x0):
     neval = 0
@@ -20,4 +20,4 @@ def nsearch(interval, tol, x0):
     answer_ = [xmin, fmin, neval, coords]
     return answer_
 
-print(nsearch(interval, tol, x0))
+#print(nsearch(interval, tol, x0))
